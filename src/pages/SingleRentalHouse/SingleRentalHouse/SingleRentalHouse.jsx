ee264d2bcd5bfbd4fd001810f6a74d1678becbf5
swapnilaanam <swapnilaanam211@gmail.com>
2023-09-24 03:19:20 +0600

    made the house details page responsive

--- src/pages/SingleRentalHouse/SingleRentalHouse/SingleRentalHouse.jsx
+++ src/pages/SingleRentalHouse/SingleRentalHouse/SingleRentalHouse.jsx
@@ -14,17 +14,17 @@ const SingleRentalHouse = () => {
     }, [id]);
 
     return (
-        <div className="w-full h-screen bg-green-50 p-24">
-            <div className="flex justify-between gap-20">
-                <div className="w-1/2 bg-yellow-200 p-3 rounded-sm shadow-lg shadow-yellow-100">
+        <div className="w-full min-h-screen bg-green-50 px-4 py-8 md:p-24">
+            <div className="flex flex-col md:flex-row justify-between gap-12 md:gap-20">
+                <div className="w-full md:w-1/2 bg-yellow-200 p-3 rounded-sm shadow-lg shadow-yellow-100">
                     <img src={house.houseImage} alt={house.houseName} className="w-full h-[375px] rounded object-cover" />
                 </div>
-                <div className="w-1/2 mt-7 space-y-5">
+                <div className="w-full md:w-1/2 mt-7 space-y-5 text-center md:text-left">
                     <h2 className="text-2xl font-semibold">{house.houseName}</h2>
                     <p className="text-lg font-medium">By {house.ownerName}</p>
-                    <div className="flex">
+                    <div className="flex flex-col md:flex-row items-center">
                         <p className="text-lg font-medium me-3">Description: </p>
-                        <div className="mt-1 flex flex-wrap justify-start items-center gap-2">
+                        <div className="mt-3 md:mt-1 flex flex-wrap justify-center md:justify-start items-center gap-2">
                             <div className="badge badge-neutral text-white">{house.bedroomNumber} Bedroom</div>
                             <div className="badge badge-neutral text-white">{house.dineNumber} Dine</div>
                             <div className="badge badge-neutral text-white">{house.kitchenNumber} kitchen</div>
@@ -32,7 +32,7 @@ const SingleRentalHouse = () => {
                             <div className="badge badge-neutral text-white">{house.bathroomNumber} Bathroom</div>
                         </div>
                     </div>
-                    <p className="text-lg font-medium flex items-center">
+                    <p className="text-lg font-medium flex justify-center md:justify-normal items-center">
                         <span className="me-3">Floor Number: </span>
                         <div className="badge badge-neutral text-white">{house.floorNumber}</div>
                     </p>
